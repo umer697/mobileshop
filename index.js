bc2d98c6e7213ae75d31f58cb19e84b14b425318
Umer Farooq <umerfarooq.697@gmail.com>
2022-08-03 18:34:45 +0500

    homePage

---
+++ index.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+
+    //banner owl carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+
+        dots: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        loop: true
+    });
+
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+
+    });
+
+    //isotope fileters
+    var $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    })
+
+    //filter items on button clicks
+    $(".button-group").on("click", "button", function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+
+    // new phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+
+    });
+
+
+    //blogs
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    })
+
+
+
+
+
+});
